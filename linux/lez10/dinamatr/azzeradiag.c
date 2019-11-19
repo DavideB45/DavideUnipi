6a530d9d1cda59e6987e5a06038ea43e1dcc6fb9
DavideB45 <dadewww@gmail.com>
2019-11-19 14:39:08 +0100

    wip

--- linux/lez10/dinamatr/azzeradiag.c
+++ linux/lez10/dinamatr/azzeradiag.c
@@ -3,13 +3,14 @@
 
 int getint(void);
 int *insert(size_t dim, int *matr);
-//void azzera(size_t dim, int matr[][]);
+void azzera(size_t dim, int *matr);
 void printmatr(size_t dim, int *matr);
 
 int main(void){
 	int *matr;
 	int dim = getint();
 	matr = insert(dim, matr);
+	azzera(dim, matr);
 	printmatr(dim, matr);
 	return 0;
 }
@@ -28,16 +29,23 @@ int *insert(size_t dim, int *matr){
 	matr = calloc(dim * dim, sizeof(int));
 	for (size_t i = 0; i < dim; i++){
 		for (size_t j = 0; j < dim; j++){
-			*((matr + j) + i) = getint();
+			*(matr + j + i*dim) = getint();
 		}
 	}
 	return matr;	
 }
 
+void azzera(size_t dim, int *matr){
+	for (size_t i = 0; i < dim; i++){
+		*(matr + i*dim + i) = 0;
+		*(matr + (dim-1) + (dim-1)*i) = 0;
+	}
+}
+
 void printmatr(size_t dim, int *matr){
 	for (size_t i = 0; i < dim; i++){
 		for (size_t j = 0; j < dim; j++){
-			printf("%d ", *(matr+i+j));
+			printf("%d ", *(matr+(i*dim)+j));
 		}
 		printf("\n");
 	}
