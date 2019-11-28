c2c3669e6cb0412cd85b537df1fa085a111d9e0c
DavideB45 <dadewww@gmail.com>
2019-11-28 19:14:31 +0100

    wip

--- linux/provevarie/readinput/variousend.c
+++ linux/provevarie/readinput/variousend.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
 #include <string.h>
 
-typedef enum{NUMBER, OPERATOR} Ind;
-struct equazione{
-	Ind indice;//1=numero 0=operatore;
-	int var;
-	struct equazione *prossimoPtr;
-};
-typedef struct equazione Eq;
-typedef Eq *EqPtr;
-
-_Bool CeckOverflaw(char value[12]);
-int CreateEq(EqPtr *headPtr);
-void AddElement(EqPtr *headPtr, EqPtr *codaPtr, int value, Ind indice);
-int Recognise(char value[12]);
+char *readString(void){
+	char tempsting[20];
+	int len;
+	scanf("%[^\n^E]", tempsting);
+	getchar();
+	strcat(tempsting, "\0");
+	int dim = 1;
+	int i = 0;
+	while(tempsting[i]!='\0'){
+		dim++;
+		i++;
+	}
+	return strcpy(calloc(dim, sizeof(char)), tempsting);
+}
 
 int main(void){
-    EqPtr headPtr = NULL;
-    while(CreateEq(&headPtr));
-    return 0;
+	char *string;
+	int coso = 1;
+	do{
+		string = readString();
+		printf("%s\n",string);
+	}while(strcmp(string,"fine")!=0);
+	return 0;
 }
