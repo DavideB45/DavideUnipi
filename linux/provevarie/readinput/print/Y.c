5f8145b3227d87a3ad63a8ce9488194bc93e8844
Davide <davide@pop-os.localdomain>
2019-12-07 10:04:58 +0100

    a

--- linux/provevarie/readinput/print/Y.c
+++ linux/provevarie/readinput/print/Y.c
@@ -16,7 +16,7 @@ int main(void){
         printf("Errore nell'input\nInserire un intero positivo dispari maggiore di 2: ");
         while(getchar()!='\n');
     }
-    head(base, hig%2);
+    head(base, hig%2+1);
    // body(base, hig%2);
 }
 
