092a33ac9b0b95dd6799284663cfb26937f53473
Davide <davide@pop-os.localdomain>
2019-12-07 09:47:44 +0100

    wish

---
+++ linux/provevarie/readinput/get/trymain.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+//lo ho fatto perche non complava un altro programma
+int main(void){
+	int num = 18;
+	printf("this is a try\t%d\n", num);
+	return 0;
+}
---
+++ linux/provevarie/readinput/print/Y.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void head(size_t dim);
+void body(size_t dim);
+
+int main(void){
+    size_t hig;
+    printf("Inserire un numero positivo maggiore di 2: ");
+    while(scanf("%d", hig)!=1 || hig<2){
+        printf("Errore nell'input\nInserire un intero positivo maggiore di 2: ");
+        while(getchar()!='\n');
+    }
+    size_t base;
+    printf("Inserire un numero positivo dispari maggiore di 2: ");
+    while(scanf("%d", base)!=1 || base<2 || base%2==0){
+        printf("Errore nell'input\nInserire un intero positivo dispari maggiore di 2: ");
+        while(getchar()!='\n');
+    }
+}
