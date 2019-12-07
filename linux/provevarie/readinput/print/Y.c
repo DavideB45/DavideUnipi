5ea33f8d0b38b31e73f59e89e081596edcfc38ec
Davide <davide@pop-os.localdomain>
2019-12-07 09:49:38 +0100

    d

--- linux/provevarie/readinput/print/Y.c
+++ linux/provevarie/readinput/print/Y.c
@@ -6,13 +6,13 @@ void body(size_t dim);
 int main(void){
     size_t hig;
     printf("Inserire un numero positivo maggiore di 2: ");
-    while(scanf("%ld", hig)!=1 || hig<2){
+    while(scanf("%lu", hig)!=1 || hig<2){
         printf("Errore nell'input\nInserire un intero positivo maggiore di 2: ");
         while(getchar()!='\n');
     }
     size_t base;
     printf("Inserire un numero positivo dispari maggiore di 2: ");
-    while(scanf("%ld", base)!=1 || base<2 || base%2==0){
+    while(scanf("%lu", base)!=1 || base<2 || base%2==0){
         printf("Errore nell'input\nInserire un intero positivo dispari maggiore di 2: ");
         while(getchar()!='\n');
     }
