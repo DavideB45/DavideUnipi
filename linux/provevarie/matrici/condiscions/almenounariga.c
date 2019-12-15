97b6fb9e0485c269bf397b02e879a702776c8e81
Davide <davide@pop-os.localdomain>
2019-12-15 10:23:13 +0100

    loop

--- linux/provevarie/matrici/condiscions/almenounariga.c
+++ linux/provevarie/matrici/condiscions/almenounariga.c
@@ -11,24 +11,33 @@ _Bool condizione(size_t righe, size_t colonne, int matrice[colonne][righe]);
 
 int main(void){
 	srand(time(NULL));
-	size_t righe = rand()%500;
-	/*printf("inserire righe: ");
-	while (scanf("%lu", &righe)!=1){
-		while(getchar()!='\n');
-	}*/
-	size_t colonne = rand()%20;
-	/*printf("inserire colonne: ");
-	while (scanf("%lu", &colonne)!=1){
-		while(getchar()!='\n');
-	}*/
-	int matrice[righe][colonne];
-	read(righe, colonne, matrice);
-	print(righe, colonne, matrice);
-	if(condizione(righe, colonne, matrice)){
-		printf("\nla condizione e' verificata\n");
-	} else {
-		printf("\nla condizione non e' verificata\n");
-	}
+	_Bool condiz = 1;
+	size_t colonne;
+	size_t righe;
+	int righegenerate = 0;
+	do{
+		righe = rand()%900;
+		righegenerate += righe;
+		/*printf("inserire righe: ");
+		while (scanf("%lu", &righe)!=1){
+			while(getchar()!='\n');
+		}*/
+		colonne = rand()%20 + 15;
+		/*printf("inserire colonne: ");
+		while (scanf("%lu", &colonne)!=1){
+			while(getchar()!='\n');
+		}*/
+		int matrice[righe][colonne];
+		read(righe, colonne, matrice);
+		print(righe, colonne, matrice);
+		condiz = condizione(righe, colonne, matrice);
+		if(condiz){
+			printf("\nla condizione e' verificata\n");
+		} else {
+			printf("\nla condizione non e' verificata\n");
+		}
+	}while(!condiz);
+	printf("sono state generate ben %d righe di numeri \n", righegenerate);
 }
 
 void read(size_t righe, size_t colonne, int matrice[righe][colonne]){
@@ -59,31 +68,22 @@ _Bool condizione(size_t righe, size_t colonne, int matrice[righe][colonne]){
 	int indiceRig = 0;
 	_Bool condizione = 0;
 	_Bool dispari = 0;
-	int nrighepari = 0;
-	while(indiceRig < righe){
+	while(!condizione && indiceRig < righe){
 		while(!dispari && indiceCol < colonne){
 			dispari = 1==(matrice[indiceRig][indiceCol])%2;
 			indiceCol++;
 		}
 		if(dispari==0 && colonne==indiceCol){//non c'e' un numero dispari
 			condizione = 1;
-			nrighepari++;
-
-			printf("\nriga = %d\n", indiceRig);
-			printf("colonna = %d\n", indiceCol);
-			indiceCol = 0;
-			indiceRig++;
-			dispari = 0;
 		} else {
 			indiceCol = 0;
 			indiceRig++;
 			dispari = 0;//dispari era '1'
 		}
 	}
-	if(indiceCol!=0){
-		printf("\nriga = %d\n", indiceRig);
-		printf("colonna = %d\n", indiceCol);
-	}
-	printf("numero = %d\n", nrighepari);
+	
+	printf("\nriga = %d\n", indiceRig);
+	printf("colonna = %d\n", indiceCol);
+	
 	return condizione;
 }
