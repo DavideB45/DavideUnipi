7525a9b5c5f2a778fa696c8f7c32508504b23ee4
DavideB45 <dadewww@gmail.com>
2020-01-24 13:02:18 +0100

    meteo.it

---
+++ py3.8/programs/meteomassa.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+page = requests.get('http://www.meteoapuane.it/previsioni.php')
+soup = BeautifulSoup(page.content, 'html.parser')
+
+legenda = {
+	'1':  'cielo coperto 1',
+	'2':  'cielo coperto 2',
+	'3':  'nuvoloso con foschia',
+	'4':  'foschia',
+	'5':  'pioggia che congela al suolo',
+	'6':  'grandine / pentagoni',
+	'7':  'variabile con neve',
+	'8':  'variabile con rovesci deboli',
+	'9':  'cielo molto nuvoloso',
+	'10': 'nebbia',
+	'11': 'nevischio',
+	'12': 'neve forte',
+	'13': 'neve mista a pioggia',
+	'14': 'neve debole',
+	'15': 'neve moderata',
+	'16': 'cielo nuvoloso',
+	'17': 'pioggia moderata',
+	'18': 'pioggia forte',
+	'19': 'pioggia debole',
+	'20': 'cielo poco nuvoloso'
+}
+giorno = ['oggi', 'domani', 'dopodomani']
+
+days = soup.find_all(id='massa')
+proxi_future = days.__len__()
+for i in range(proxi_future):
+	if i % 2 == 0:
+		print(giorno[int(i/2)],' mattina:')
+	else:
+		print(giorno[int(i/2)],' pomeriggio/sera:')
+	day_1 = (str(days[i])).partition('<div id="massa"><img alt="*" src="img/previsioni/')
+	day_1 = day_1[2].partition('.')[0]
+	print(legenda[day_1])
+	if i % 2 == 1:
+		print('\n------------------\n')
+	else:
+		print()
--- py3.8/programs/qualcosa_con_internet.py
+++ py3.8/programs/qualcosa_con_internet.py
@@ -3,24 +3,44 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-
+#cerca la pagina
 page = requests.get('https://forecast.weather.gov/MapClick.php?lat=28.56604489500006&lon=-81.68864878999995#.XinRGshKhhE')
 #serve per semplificare la ricerca
 soup = BeautifulSoup(page.content, 'html.parser')
 #soup.find_all('a') links
 
 #copio la cosa che mi interessa
+#e' tutto in html
+#per trovare l'oggetto basta ispezionare il sito
+#in html e andare con il mouse sulla cosa che vogliamo
+
+#la parte con le immagini del cielo 
 week = soup.find(id='seven-day-forecast-body')
+#la lista delle singole immagini(non le immagini gli oggetti)
 items = week.find_all(class_ = 'tombstone-container')
-#print(items[1])
 
-print(items[0].find(class_='period-name').get_text())
-print(items[0].find(class_='short-desc').get_text())
-print(items[0].find(class_='temp temp-high').get_text())
 
+
+#esempio di come si cerca un pezzetto
+#per farne una stringa
+#print(items[0].find(class_='temp temp-high').get_text())
+
+
+
+#quando
+print(items[0])
 period_names = [item.find(class_='period-name').get_text() for item in items]
+#come e' il tempo in parole
 short_desc = [item.find(class_='short-desc').get_text() for item in items]
+#temperatura
 temp = [item.find(class_='temp').get_text() for item in items]
+print(temp)
+
+#temp format = '(High|low): %d °F'
+for i in range(temp.__len__()):
+	temperatuta_x = temp[i].split(' ')#divido la temperatura dal resto
+	temperatuta_x[1] = str(int((int(temperatuta_x[1])-32)/1.8))#tresformo in °C
+	temp[i] = ' '.join([temperatuta_x[1], '°C'])#gli do il formato piu' bello
 
 weather_stuff = pd.DataFrame({
 	'qunado': period_names,
@@ -28,6 +48,7 @@ weather_stuff = pd.DataFrame({
 	'temperature': temp
 })
 
+
 print(weather_stuff)
 
-weather_stuff.to_csv('weather.csv')
+#weather_stuff.to_csv('weather.csv')
