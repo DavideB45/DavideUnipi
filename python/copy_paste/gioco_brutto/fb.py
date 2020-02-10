c54937832953d2ef640b575b985dc1b9bda7fd0b
Davide <davide@pop-os.localdomain>
2020-02-10 13:01:34 +0100

    :(

--- python/copy_paste/gioco_brutto/fb.py
+++ python/copy_paste/gioco_brutto/fb.py
@@ -4,16 +4,20 @@ import os
 
 
 pygame.init()
+clock = pygame.time.Clock()
+clock.tick(60)
+
 
 #load img
 dirname = os.path.dirname(os.path.abspath(__file__))
-
 bird = pygame.image.load(os.path.join(dirname, 'bestia'))
 bird = pygame.transform.scale(bird, (57,50))
-
 bg = pygame.image.load(os.path.join(dirname, 'bg.png'))
 bg = pygame.transform.scale(bg, (400,600))
-
+ground = pygame.image.load(os.path.join(dirname, 'ground.png'))
+ground = pygame.transform.scale(ground, (500, 112))
+death = pygame.image.load(os.path.join(dirname, 'gov.jpg'))
+death = pygame.transform.scale(death , (200, 200))
 
 #gb
 WN = pygame.display.set_mode((400,600))
@@ -25,21 +29,44 @@ def init():
 	v_y = 0
 init()
 
+def wait4q():
+	waiting = True
+	while waiting:
+		for event in pygame.event.get():
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_SPACE:
+					pygame.quit()
+					exit()
+				if event.key == pygame.K_q:
+					waiting = False
+					init()		
+
 def draw():
 	WN.blit(bg, (0,0))
-	WN.blit(bird, (x_pos, y_pos))
+	WN.blit(bird, (5, int(y_pos)))
+	x_pos = 1
+	if x_pos > 50:
+		x_pos = 0
+	WN.blit(ground, (x_pos,500))
+	if y_pos > 488:
+		WN.blit(death, (100, 0))
+		wait4q()
 
 def update_wn():
 	pygame.display.update()
 	#pygame.Clock,time(FPS)
 
+v_y = 0.5
 go_on = True
 while go_on:
-	v_y += 1
 	y_pos += v_y
+	v_y = 0.1
 	draw()
 	update_wn()
 	for event in pygame.event.get():
-		if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-			pygame.quit()
-			go_on = False
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_SPACE:
+				pygame.quit()
+				go_on = False
+			if event.key == pygame.K_1:
+				v_y = -50
