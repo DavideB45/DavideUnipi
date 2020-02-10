24ab32e15aa94d57ffd0a39a5c1a800992993754
Davide <davide@pop-os.localdomain>
2020-02-10 12:18:11 +0100

    uff.py

--- python/copy_paste/gioco_brutto/fb.py
+++ python/copy_paste/gioco_brutto/fb.py
@@ -7,8 +7,12 @@ pygame.init()
 
 #load img
 dirname = os.path.dirname(os.path.abspath(__file__))
+
 bird = pygame.image.load(os.path.join(dirname, 'bestia'))
-#bird = pygame.image.load('gov')
+bird = pygame.transform.scale(bird, (57,50))
+
+bg = pygame.image.load(os.path.join(dirname, 'bg.png'))
+bg = pygame.transform.scale(bg, (400,600))
 
 
 #gb
@@ -22,14 +26,20 @@ def init():
 init()
 
 def draw():
+	WN.blit(bg, (0,0))
 	WN.blit(bird, (x_pos, y_pos))
 
 def update_wn():
 	pygame.display.update()
-	#pygame.time.Clock.tick(FPS)
+	#pygame.Clock,time(FPS)
 
-while True:
-	#v_y += 1
-	#y_pos += v_y
+go_on = True
+while go_on:
+	v_y += 1
+	y_pos += v_y
 	draw()
 	update_wn()
+	for event in pygame.event.get():
+		if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+			pygame.quit()
+			go_on = False
