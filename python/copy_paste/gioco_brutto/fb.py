56f997739b1977bec8776db6d7e278f697a7e90b
Davide <davide@pop-os.localdomain>
2020-02-11 13:38:19 +0100

    e' "finito"

--- python/copy_paste/gioco_brutto/fb.py
+++ python/copy_paste/gioco_brutto/fb.py
@@ -7,7 +7,6 @@ pygame.init()
 clock = pygame.time.Clock()
 clock.tick(60)
 
-
 #load img
 dirname = os.path.dirname(os.path.abspath(__file__))
 bird = pygame.image.load(os.path.join(dirname, 'bestia'))
@@ -18,15 +17,43 @@ ground = pygame.image.load(os.path.join(dirname, 'ground.png'))
 ground = pygame.transform.scale(ground, (500, 112))
 death = pygame.image.load(os.path.join(dirname, 'gov.jpg'))
 death = pygame.transform.scale(death , (200, 200))
+pipe = pygame.image.load(os.path.join(dirname, 'pipe.png'))
+pipe_down = pygame.transform.scale(pipe, (60, 220))
+pipe_up = pygame.transform.flip(pipe_down, False, True)
 
 #gb
 WN = pygame.display.set_mode((400,600))
 FPS = 50
 
+
+class tubi:
+	def __init__(self):
+		self.x = 400
+		self.y = random.randint(300, 420)
+	def move(self):
+		self.x -= 0.1
+		WN.blit(pipe_down, (self.x, self.y))
+		WN.blit(pipe_up, (self.x, self.y - 380))
+	def ceck_hit(self, y_pos):
+		b_dx = 5 + bird.get_width()
+		b_sx = 5
+		print(self.y - 160, y_pos)
+		'''y_pos + bird.get_height() < self.y and'''
+		if y_pos + bird.get_height() > self.y or y_pos < self.y -160:
+			if b_sx < self.x + pipe.get_width() and b_dx > self.x:
+				sconfitta()
+
 def init():
 	global x_pos, y_pos, v_y
 	x_pos, y_pos = 0, 0
 	v_y = 0
+	global ostacoli
+	ostacoli = []
+	ostacoli.append(tubi())
+	ostacoli.append(tubi())
+	ostacoli.append(tubi())
+	ostacoli[1].x = 600
+	ostacoli[2].x = 800
 init()
 
 def wait4q():
@@ -41,16 +68,26 @@ def wait4q():
 					waiting = False
 					init()		
 
+def sconfitta():
+	WN.blit(death, (100, 0))
+	pygame.display.update()
+	wait4q()
+
 def draw():
 	WN.blit(bg, (0,0))
+	for tubo in ostacoli:
+		tubo.move()
+	if ostacoli[0].x < -60:
+		to_kill = ostacoli[0]
+		ostacoli.remove(to_kill)
+		ostacoli.append(tubi())
+		ostacoli[-1].x = ostacoli[-2].x + random.randint(180, 250)
 	WN.blit(bird, (5, int(y_pos)))
-	x_pos = 1
-	if x_pos > 50:
-		x_pos = 0
-	WN.blit(ground, (x_pos,500))
+	WN.blit(ground, (int(x_pos),500))
+	pygame.display.update()
+	ostacoli[0].ceck_hit(y_pos)
 	if y_pos > 488:
-		WN.blit(death, (100, 0))
-		wait4q()
+		sconfitta()
 
 def update_wn():
 	pygame.display.update()
@@ -60,9 +97,12 @@ v_y = 0.5
 go_on = True
 while go_on:
 	y_pos += v_y
+	x_pos -= 0.1
+	if x_pos < -100:
+		x_pos = 0
 	v_y = 0.1
-	draw()
 	update_wn()
+	draw()
 	for event in pygame.event.get():
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_SPACE:
