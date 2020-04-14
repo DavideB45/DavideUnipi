887fcdf16e18ed595f8d72739bee977034883465
DavideB45 <dadeww@gmail.com>
2020-04-14 16:36:44 +0200

    wip

--- algo/lez7/k_stringhe/piufreq.c
+++ algo/lez7/k_stringhe/piufreq.c
@@ -2,113 +2,50 @@
 #include <string.h>
 #include <stdlib.h>
 
-typedef struct wcount{
-	char *stringa;
-	int tot;
-}wcount;
+typedef struct stringa{
+	char *str;
+	int count;
+}stringa;
 
 int readInt(void);
-void fillarray(int dim, char **array);
-void printarray(int dim, char **array){
-	for (size_t i = 0; i < dim; i++){
-		printf("%s\n", array[i]);
-	}
-	
-}
+void fillArray(int dim, char *array[dim]);
+void printArray(int dim, char *array[dim]);
+int compare(void *A, void *B);
 
-int partition(int inizio, int fine, char **array);
-void quickSotr(int inizio,int fine, char **array);
-
-void find_top(int dim, int number, char **array);
 
 int main(void){
-	int n_string = readInt();
-	int n_winners = readInt();
+	int tot = readInt();
+	int top = readInt();
 
-	char *array[n_string];
-	fillarray(n_string, array);
-	quickSotr(0, n_string - 1, array);
-//	puts("");
-	printarray(n_string, array);
-	find_top(n_string, n_winners, array);
+	char *parole[tot];
+	fillArray(tot, parole);
 
-	return 0;
+	qsort(parole, tot, sizeof(char*), compare);
+
+
 }
+
 
 int readInt(void){
 	int num;
 	while(scanf("%d", &num) != 1){
-		printf("ritenta : ");
-		while(getchar() != '\n'){}
+		printf("metti un numero : ");
+		while(getchar() != '\n');
 	} while(getchar() != '\n');
 	return num;
 }
-void fillarray(int dim, char **array){
-	for (size_t i = 0; i < dim; i++){
-		array[i] = (char*) malloc(101*sizeof(char));
+
+void fillArray(int dim, char *array[dim]){
+	for(size_t i = 0; i < dim; i++){
 		scanf("%s", array[i]);
 	}
 }
 
-int partition(int inizio, int fine, char **array){
-	int piv = fine;
-	int j = inizio - 1;
-	char *temp;
-	for (size_t i = inizio; i < fine; i++){
-		if(strcmp(array[i], array[piv]) < 0){
-			j++;
-			temp = array[j];
-			array[j] = array[i];
-			array[i] = temp; 
-		}
-	}
-	j++;
-	temp = array[j];
-	array[j] = array[piv];
-	array[piv] = temp; 
-	
-	return j;
-}
-void quickSotr(int inizio,int fine, char **array){
-	if(inizio < fine){
-		int q = partition(inizio, fine, array);
-		quickSotr(inizio, q-1, array);
-		quickSotr(q+1, fine, array);
-	}
+int compare(void *A, void *B){
+	char *APtr = (char*) A;
+	char *BPtr = (char*) B;
+	return strcmp(APtr, BPtr);
 }
 
-void find_top(int dim, int number, char **array){
-	wcount *vincitori = calloc(number, sizeof(wcount));;
-	int tot = 1, j = number - 1;
-	for (size_t i = 0; i < dim - 1; i++){
-		while(i < dim && strcmp(array[i], array[i + 1]) == 0){
-			tot++;
-			i++;
-		}
-		while(j > 0 && vincitori[j].tot < tot){
-	
-			for (size_t i = 0; i < number; i++){
-				printf("%s -> %d\n", vincitori[i].stringa, vincitori[i].tot);
-			} printf("\n");
+void printArray(int dim, char *array[dim]);
 
-			vincitori[j].stringa = vincitori[j - 1].stringa;
-			vincitori[j].tot = vincitori[j - 1].tot;
-			j--;
-		}
-		if(tot < vincitori[j].tot){
-			j++;
-		}
-		if(j < number){
-			vincitori[j].tot = tot;
-			vincitori[j].stringa = array[i];
-		}
-		j = number - 1;
-		tot = 1;
-	
-	}
-	printf("\n");
-	for (size_t i = 0; i < number; i++){
-		printf("%s -> %d\n", vincitori[i].stringa, vincitori[i].tot);
-	}
-	
-}
