6fff056f8b314c26c15e1769d09d61b808e03ad7
DavideB45 <dadeww@gmail.com>
2020-04-14 18:02:39 +0200

    non so programmare

--- algo/lez7/k_stringhe/piufreq.c
+++ algo/lez7/k_stringhe/piufreq.c
@@ -9,9 +9,10 @@ typedef struct stringa{
 
 int readInt(void);
 void fillArray(int dim, char *array[dim]);
-void printArray(int dim, char *array[dim]);
-int compare(void *A, void *B);
-
+void printArray(int dim, int top, char *array[dim]);
+int comparestr(const void *A,const void *B);
+int comparetot(const void *A,const void *B);
+int lastcompare(const void *A,const void *B);
 
 int main(void){
 	int tot = readInt();
@@ -20,9 +21,9 @@ int main(void){
 	char *parole[tot];
 	fillArray(tot, parole);
 
-	qsort(parole, tot, sizeof(char*), compare);
+	qsort(parole, tot, sizeof(char*), comparestr);
 
-
+	printArray(tot, top, parole);
 }
 
 
@@ -37,15 +38,46 @@ int readInt(void){
 
 void fillArray(int dim, char *array[dim]){
 	for(size_t i = 0; i < dim; i++){
+		array[i]  = malloc(101*sizeof(char));
 		scanf("%s", array[i]);
 	}
 }
 
-int compare(void *A, void *B){
-	char *APtr = (char*) A;
-	char *BPtr = (char*) B;
+int comparestr(const void *A,const void *B){
+	char *APtr = *( (char**) A);
+	char *BPtr = *( (char**) B);
 	return strcmp(APtr, BPtr);
 }
 
-void printArray(int dim, char *array[dim]);
+int comparetot(const void *A,const void *B){
+	return ((stringa*) B)->count - ((stringa*) A)->count;
+}
+
+int lastcompare(const void *A,const void *B){
+	return strcmp(((stringa*) A)->str , ((stringa*) B)->str);
+}
+
+void printArray(int dim, int top, char *array[dim]){
+	stringa frequenza[dim];
+
+	int i = 0, j = -1;
+	while(i < dim){
+		if(i > 0 && strcmp(array[i], (frequenza)[j].str) == 0){
+			(frequenza)[j].count++;
+		} else {
+			j++;
+			(frequenza)[j].count = 1;
+			(frequenza)[j].str = array[i];
+		}
+		i++;
+	}
+	
+	qsort(frequenza, j + 1, sizeof(stringa), comparetot);
+	qsort(frequenza, top, sizeof(stringa), lastcompare);
+	
+	for (size_t i = 0; i < top; i++){
+		printf("%s\n", (frequenza)[i].str);
+	}
+	
+}
 
